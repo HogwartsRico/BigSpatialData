da3b7d3b223358f92e4b585aa5695658787b5e1a
Santing <zsanting@163.com>
2017-02-16 23:11:15 +0800

    1、实现临时数据表导入，即将未构建索引的数据导入至NoneIndexData表中，NoneIndexData表用于索引构建

--- src/main/java/edu/jxust/SpatialData/DataTable.java
+++ src/main/java/edu/jxust/SpatialData/DataTable.java
@@ -9,11 +9,18 @@
 package edu.jxust.SpatialData;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+
 
 /** 
 * @ClassName: DataTable 
@@ -23,6 +30,43 @@ import org.apache.hadoop.hbase.client.Put;
 *  
 */
 public class DataTable {
+
+	public static void importNoneIndexData(HTableInterface tableSpatial, HTableInterface tableNoneIndexData)
+			throws Exception {
+		Scan scan = new Scan();
+		ResultScanner scanner = tableSpatial.getScanner(scan);
+		Result result = scanner.next();
+		int count = 0;
+		long total = 0;
+		byte[] fDataKey = "DataKey".getBytes();
+		byte[] cKey = "Key".getBytes();
+		List<Put> puts = new ArrayList<>();
+		while (result != null) {
+			count++;
+			String numStr = StringUtils.leftPad(Long.toString(total), 18, '0');
+			Put put = new Put(numStr.getBytes());
+			put.add(fDataKey, cKey, result.getRow());
+			puts.add(put);
+
+			result = scanner.next();
+			if (count >= 10000 && result != null) {
+				tableNoneIndexData.put(puts);
+				scan.setStartRow(result.getRow());
+				scanner.close();
+				scanner = tableSpatial.getScanner(scan);
+				scanner.next();
+				count = 0;
+				puts = new ArrayList<>();
+				System.out.println(String.format("共导入记录：%s", total));
+			}
+			total++;
+		}
+		tableNoneIndexData.put(puts);
+		System.out.println(String.format("共导入记录：%s", total));
+		scanner.close();
+		tableSpatial.close();
+		tableNoneIndexData.close();
+	}
 
 	public void importCSVFile(HTableInterface table, String file, String fileHeader) {
 
