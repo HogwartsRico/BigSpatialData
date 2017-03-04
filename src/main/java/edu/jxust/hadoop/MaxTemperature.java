4a6ee5a8518f47183e1ddf3efe17eded8af1cf91
Santing <zsanting@163.com>
2017-03-04 14:30:59 +0800

    1、MapReduce编程测试

---
+++ src/main/java/edu/jxust/hadoop/MaxTemperature.java
@@ -0,0 +1,73 @@
+package edu.jxust.hadoop;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.log4j.Logger;
+
+public class MaxTemperature {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		// String uri="hdfs://192.168.128.1:9000/"; //hdfs ��ַ
+		// String local="G:\\TDDOWNLOAD\\jdk-7u79-linux-x64.rpm"; //����·��
+		// String remote="hdfs://192.168.128.1:9000/tim";
+		// Configuration conf = new Configuration();
+		// conf.set("dfs.blocksize","67108864");
+		// copyFile(conf,uri,local,remote);
+		Logger.getLogger(MaxTemperature.class);
+		maxTemperature();
+	}
+
+	private static void maxTemperature() throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("dfs.blocksize", "67108864");
+		Job job = Job.getInstance(conf, "MaxTemp");
+		job.setJarByClass(MaxTemperature.class);
+
+		job.setJobName("Max Temperature");
+		/*
+		 * String inPath = "hdfs://192.168.128.1:9000/tim/ncdc"; String outPath
+		 * = "hdfs://192.168.128.1:9000/tim/MaxTemperatureout3";
+		 */
+		String inPath = "hdfs://192.168.128.1:9000/tim/ncdc";
+		String outPath = String.format("hdfs://192.168.128.1:9000/tim/test/MaxTemperatureout_%s",
+				System.currentTimeMillis());
+
+		FileInputFormat.addInputPath(job, new Path(inPath));
+		FileOutputFormat.setOutputPath(job, new Path(outPath));
+
+		job.setMapperClass(MaxTemperatureMapper.class);
+
+		job.setReducerClass(MaxTemperatureReducer.class);
+
+		job.setMapOutputKeyClass(Text.class);
+
+		job.setMapOutputValueClass(IntWritable.class);
+		System.out.println(job.waitForCompletion(true));
+	}
+
+	/**
+	 * �ϴ��ļ�
+	 * 
+	 * @param conf
+	 * @param local
+	 * @param remote
+	 * @throws IOException
+	 */
+	public static void copyFile(Configuration conf, String uri, String local, String remote) throws IOException {
+		FileSystem fs = FileSystem.get(URI.create(uri), conf);
+		fs.copyFromLocalFile(new Path(local), new Path(remote));
+		System.out.println("copy from: " + local + " to " + remote);
+		fs.close();
+	}
+
+}
---
+++ src/main/java/edu/jxust/hadoop/MaxTemperatureReducer.java
@@ -0,0 +1,29 @@
+package edu.jxust.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * Reducer��
+ * @author GIS
+ *
+ */
+public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+	@Override
+	protected void reduce(Text keyin, Iterable<IntWritable> valuein,Context context) throws IOException, InterruptedException {
+	
+		int maxValue = Integer.MIN_VALUE;
+		for (IntWritable value : valuein) {
+			maxValue = Math.max(maxValue, value.get());
+			System.out.println(String.format("%s:%d",keyin, value.get()));
+		}
+		context.write(new Text(String.format("����%s", keyin)), new IntWritable(maxValue));
+		System.out.println(String.format("���->%s:%d", keyin,maxValue));
+		context.write(keyin, new IntWritable(maxValue));
+	}
+
+}
