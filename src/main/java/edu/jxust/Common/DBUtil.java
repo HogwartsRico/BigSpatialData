a540b4e1acd565c9dc6536510030fc8af56c0665
Santing <zsanting@163.com>
2017-03-05 15:38:17 +0800

    1、数据连接配置通用操作类

--- src/main/java/edu/jxust/Common/DBUtil.java
+++ src/main/java/edu/jxust/Common/DBUtil.java
@@ -8,17 +8,18 @@
 */
 package edu.jxust.Common;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.io.InputStream;
+
 import java.util.Properties;
 
 /** 
 * @ClassName: DBUtil 
-* @Description: TODO
+* @Description: jdbc数据操作类
 * @author 张炫铤
 * @date 2017年2月16日 上午9:30:20 
 *  
@@ -26,37 +27,38 @@ import java.util.Properties;
 public class DBUtil {
 	// 定义链接所需要的变量
 	private static Connection con = null;
-	private static PreparedStatement ps = null;
-	private static ResultSet rs = null;
+	// private static PreparedStatement ps = null;
+	// private static ResultSet rs = null;
 
 	// 定义链接数据库所需要的参数
 	private static String url = "";
 	private static String username = "";
 	private static String driver = "";
 	private static String password = "";
-
+	private static Boolean isLongTimeRunning = false;
 	// 定义读取配置文件所需要的变量
 	private static Properties dbProperties = null;
-	private static InputStream fis = null;
+	// private static InputStream fis = null;
 
 	/** 
 	 * 加载驱动 
 	 */
 	static {
+		InputStream fis = null;
 		try {
 			// 从dbinfo.properties配置文件中读取配置信息
 			dbProperties = new Properties();
-			fis = DBUtil.class.getClassLoader().getResourceAsStream("src/main/resources/DataBase.properties");
-
+			// fis =
+			// DBUtil.class.getResourceAsStream("/DataBase.properties");//src/main
+			fis = DBUtil.class.getClassLoader().getResourceAsStream("DataBase.properties");// src/main
+			// DBUtil.class.getClassLoader()
 			dbProperties.load(fis);
 			url = dbProperties.getProperty("url");
 			username = dbProperties.getProperty("username");
-			driver = dbProperties.getProperty("driver");
+			driver = dbProperties.getProperty("driver").trim();
 			password = dbProperties.getProperty("password");
-
-			// 加载驱动
+			isLongTimeRunning = Boolean.valueOf(dbProperties.getProperty("longRunning"));
 			Class.forName(driver);
-
 		} catch (Exception e) {
 			System.out.println("驱动加载失败！");
 			e.printStackTrace();
@@ -80,6 +82,12 @@ public class DBUtil {
 
 		try {
 			// 建立连接
+			if (isLongTimeRunning) {
+				url = String.format("%s:longRunning", url);
+			} else {
+				url = String.format("%s:shortRunning", url);
+			}
+
 			con = DriverManager.getConnection(url, username, password);
 
 		} catch (Exception e) {
@@ -97,6 +105,31 @@ public class DBUtil {
 	 * @param ct 
 	 */
 	public static void close(ResultSet rs, Statement ps, Connection con) {
+
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (ps != null) {
+			try {
+				ps.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (con != null) {
+			try {
+				con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(ResultSet rs, PreparedStatement ps, Connection con) {
 
 		if (rs != null) {
 			try {
