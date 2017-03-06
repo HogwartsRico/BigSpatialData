940b152600bb9250b50d2ce670a5865cd49b8f2c
Santing <zsanting@163.com>
2017-03-06 20:25:05 +0800

    1、修改lang

--- src/main/java/edu/jxust/SpatialData/DataTable.java
+++ src/main/java/edu/jxust/SpatialData/DataTable.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
